e0b721b3361dbfb83958c5aa7e38990422c6e97a
vincidl1 <VINCENZO.DELUCA3@bakerhughes.com>
2023-02-17 14:54:33 +0100

    protocol change

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const app = require('express')();
 const https = require('https').Server(app);
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(https);
 let ConnectedUser = require('./models/connected-user');
-const fs = require("fs")
+const fs = require("fs");
 const path = require("path");
 
 
